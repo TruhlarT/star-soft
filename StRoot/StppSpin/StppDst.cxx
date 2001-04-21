66608dabb4990bd5b5209e4bbe926a17df7c21b9
Yuri Fisyak <fisyak@bnl.gov>
2001-04-21 00:43:12 +0000

    *** empty log message ***

---
+++ StRoot/StppSpin/StppDst.cxx
@@ -0,0 +1,22 @@
+//*-- Author : Jan Balewski
+//  
+// $Id: StppDst.cxx,v 1.1.1.1 2001/04/21 00:43:15 fisyak Exp $
+// $Log: StppDst.cxx,v $
+// Revision 1.1.1.1  2001/04/21 00:43:15  fisyak
+// *** empty log message ***
+//
+// Revision 1.1  2001/04/19 21:40:09  balewski
+// *** empty log message ***
+//
+// First release
+//
+//
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
+
+
+#include "StppDst.h"
+TableClassImpl(ppDst,ppDst_t);
+
+
