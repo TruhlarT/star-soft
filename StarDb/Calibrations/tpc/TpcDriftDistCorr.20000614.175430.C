bbe7244d35d6bebb6f364cdd17cfceda80f47432
Yuri Fisyak <fisyak@bnl.gov>
2001-12-11 13:29:47 +0000

    First dE/dx calibration for Y2001

--- StarDb/Calibrations/tpc/TpcDriftDistCorr.20000614.175430.C
+++ StarDb/Calibrations/tpc/TpcDriftDistCorr.20000614.175430.C
@@ -6,16 +6,16 @@ TDataSet *CreateTable() {
 // ------  Test whether this table share library was loaded ------
   if (!gROOT->GetClass("St_TpcDriftDistCorr")) return 0;
 TpcDriftDistCorr_st row;
-St_TpcDriftDistCorr *tableSet = new St_TpcDriftDistCorr("TpcDriftDistCorr",48);
+St_TpcDriftDistCorr *tableSet = new St_TpcDriftDistCorr("TpcDriftDistCorr",2);
 //
 memset(&row,0,tableSet->GetRowSize());
- row.a[0]	 = -1.83196e-02; // Outer
+ row.a[0]	 =  6.43281e-02;//-1.83196e-02; // Outer
     row.a[1]	 =  3.10995e-04;
     row.a[2]	 = -7.82141e-07;
     row.a[3]	 =  0.0        ;
 tableSet->AddAt(&row);
 memset(&row,0,tableSet->GetRowSize());
-    row.a[0]	 = -4.11073e-02; // Inner
+ row.a[0]	 =  4.15404e-02;//-4.11073e-02; // Inner
     row.a[1]	 =  8.59165e-04;
     row.a[2]	 = -2.67150e-06; 
     row.a[3]	 =  0.0;
