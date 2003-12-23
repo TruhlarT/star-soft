62a8a7fe1286397be8d85e3dd34e24aeaa14b225
Yuri Fisyak <fisyak@bnl.gov>
2003-12-23 14:54:43 +0000

    ATLAS version of mortran (geant3 => agetof)

--- asps/Simulation/agetof/readlink.c
+++ asps/Simulation/agetof/readlink.c
@@ -1,8 +1,8 @@
 /*********************************************************
-* $Id: readlink.c,v 1.1 2003/12/23 14:54:43 fisyak Exp $
+* $Id: readlink.c,v 1.1.1.1 2003/12/23 14:54:43 fisyak Exp $
 * $Log: readlink.c,v $
-* Revision 1.1  2003/12/23 14:54:43  fisyak
-* Initial revision
+* Revision 1.1.1.1  2003/12/23 14:54:43  fisyak
+* ATLAS version of mortran (geant3 => agetof)
 *
 * Revision 1.1  2001/02/28 22:56:17  nevski
 * improved comment treatment, f77-structures etc
