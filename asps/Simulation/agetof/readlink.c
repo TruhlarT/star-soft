8f46c5a3c00dfd964b63b2b91b648714bbf36717
Yuri Fisyak <fisyak@bnl.gov>
2004-01-25 01:36:12 +0000

    Clean up unused variable to avoid waning by Victor request

--- asps/Simulation/agetof/readlink.c
+++ asps/Simulation/agetof/readlink.c
@@ -1,8 +1,11 @@
 /*********************************************************
-* $Id: readlink.c,v 1.1 2003/12/23 14:54:43 fisyak Exp $
+* $Id: readlink.c,v 1.2 2004/01/25 01:36:12 fisyak Exp $
 * $Log: readlink.c,v $
-* Revision 1.1  2003/12/23 14:54:43  fisyak
-* Initial revision
+* Revision 1.2  2004/01/25 01:36:12  fisyak
+* Clean up unused variable to avoid waning by Victor request
+*
+* Revision 1.1.1.1  2003/12/23 14:54:43  fisyak
+* ATLAS version of mortran (geant3 => agetof)
 *
 * Revision 1.1  2001/02/28 22:56:17  nevski
 * improved comment treatment, f77-structures etc
@@ -11,6 +14,7 @@
 * support for the true filename search
 *********************************************************/
 #include <unistd.h>
+#include <string.h>
 int readlink_(char* a, char *b, int La, int Lb)
 {
    char path[256];
