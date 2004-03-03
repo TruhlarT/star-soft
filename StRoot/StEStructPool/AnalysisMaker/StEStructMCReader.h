0631217eefe5c92f46b09414b10f601ede8d45f0
chunhuih <chunhuih>
2004-03-03 23:17:10 +0000

    added mNentries to store the total entries in the rootuple, to reduce redundant code.

--- StRoot/StEStructPool/AnalysisMaker/StEStructMCReader.h
+++ StRoot/StEStructPool/AnalysisMaker/StEStructMCReader.h
@@ -1,6 +1,6 @@
 /*********************************************************************
  *
- *  $Id: StEStructMCReader.h,v 1.2 2004/03/02 21:35:10 chunhuih Exp $
+ *  $Id: StEStructMCReader.h,v 1.3 2004/03/03 23:17:10 chunhuih Exp $
  *
  *  Author: Chunhui Han
  *
@@ -15,6 +15,11 @@
  ********************************************************************
  *
  *  $Log: StEStructMCReader.h,v $
+ *  Revision 1.3  2004/03/03 23:17:10  chunhuih
+ *
+ *  added mNentries to store the total entries in the rootuple, to reduce
+ *  redundant code.
+ *
  *  Revision 1.2  2004/03/02 21:35:10  chunhuih
  *
  *  added impact parameter information to the StEStructEvent
@@ -82,6 +87,7 @@ class StEStructMCReader : public StEStructEventReader {
   int mloopIndex;
   bool mAmDone;
   int mrefMult;
+  int mNentries;
 
   StEStructEventCuts *mECuts;
   StEStructTrackCuts *mTCuts;
@@ -89,11 +95,13 @@ class StEStructMCReader : public StEStructEventReader {
   void fillTracks(StEStructEvent *estructEvent);
   int getTotalEventCount();
   int getCharge(int);
+
   const int mIPMAX;
 
  public:
   StEStructMCReader(TTree *tree=0);
   StEStructMCReader(int nevents, TTree *tree = 0, StEStructEventCuts *ecuts = 0, StEStructTrackCuts *tcuts = 0);
+  StEStructMCReader(int nevents, char *fileListFile, StEStructEventCuts *ecuts = 0, StEStructTrackCuts *tcuts = 0);
   ~StEStructMCReader();
 
   void setEventCuts(StEStructEventCuts *cuts);
