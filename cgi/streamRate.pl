c3da589387f49b36ad0ce4ec962fd0d79af26d63
Lidia Didenko <didenko@bnl.gov>
2010-09-15 15:27:13 +0000

    minor fixes

--- cgi/streamRate.pl
+++ cgi/streamRate.pl
@@ -4,7 +4,7 @@
 # $Id:
 #
 # L. Didenko 
-# streamCpuPlots.pl to make plots for average RealTime/CPU job's usage 
+# streamRate.pl to make plots for average RealTime/CPU job's usage 
 #
 ##########################################################
 
