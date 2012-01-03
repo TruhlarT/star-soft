af9ac644408f6207595df27569795dd3ab261734
Lidia Didenko <didenko@bnl.gov>
2012-01-03 20:01:07 +0000

    change skip label

--- cgi/CRSstatusPlots.pl
+++ cgi/CRSstatusPlots.pl
@@ -1,11 +1,11 @@
 #!/usr/local/bin/perl
 #!/usr/bin/env perl 
 #
-# $Id: CRSstatusPlots.pl,v 1.25 2012/01/03 19:57:46 didenko Exp $
+# $Id: CRSstatusPlots.pl,v 1.27 2012/01/03 20:01:07 didenko Exp $
 #
 # $Log: CRSstatusPlots.pl,v $
-# Revision 1.25  2012/01/03 19:57:46  didenko
-# add 2012
+# Revision 1.27  2012/01/03 20:01:07  didenko
+# change skip label
 #
 # Revision 1.24  2011/01/04 19:33:39  didenko
 # updated for 2011 year
@@ -297,8 +297,8 @@ if ( ! $graph){
  my $xLabelPosition = 0;
  my $xLabelSkip = 1;
 
-$xLabelSkip = 1  if( $fperiod eq "day" );
-$xLabelSkip = 4 if( $fperiod eq "week" );
+$xLabelSkip = 2  if( $fperiod eq "day" );
+$xLabelSkip = 8 if( $fperiod eq "week" );
 $xLabelSkip = 24 if( $fperiod eq "1_month" );
 $xLabelSkip = 48 if( $fperiod eq "2_months" );
 $xLabelSkip = 72 if( $fperiod eq "3_months" );
