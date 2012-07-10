71cfdf78926e5e6069ccb5531a6a2ad8a65f15cf
Lidia Didenko <didenko@bnl.gov>
2012-07-10 18:49:06 +0000

    minor changes

--- cgi/jobsSummary2012.pl
+++ cgi/jobsSummary2012.pl
@@ -3,7 +3,7 @@
 # $Id:
 #
 # L.Didenko
-# jobSummary2011.pl - summary of production jobs status for year 2012
+# jobSummary2012.pl - summary of production jobs status for year 2012
 #
 ########################################################################################
 
